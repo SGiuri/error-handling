627ece18e980b5eb977c1308c5fd82f004d1d036
SGiuri <s.giuri@issm.it>
2020-10-26 22:26:19 +0100

    On my way

---
+++ error_handling.py
@@ -0,0 +1,20 @@
+def handle_error_by_throwing_exception():
+    raise ValueError("Something wrong")
+    pass
+
+
+def handle_error_by_returning_none(input_data):
+    try:
+        a = int(input_data)
+        return a
+    except:
+        return None
+    pass
+
+
+def handle_error_by_returning_tuple(input_data):
+    pass
+
+
+def filelike_objects_are_closed_on_exception(filelike_object):
+    pass
