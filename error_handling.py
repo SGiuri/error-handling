e45faf20ff84ad8409479a155377b28dd636e674
SGiuri <s.giuri@issm.it>
2020-10-27 13:05:27 +0100

    work done

--- error_handling.py
+++ error_handling.py
@@ -22,4 +22,11 @@ def handle_error_by_returning_tuple(input_data):
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
+    filelike_object.open()
+    try:
+        filelike_object.do_something()
+    except:
+        filelike_object.close()
+        handle_error_by_throwing_exception()
+    filelike_object.close()
     pass
